476fe03f812f2a9009744e27f1335acfce441454
Minsun Kim <75118893+mins-n@users.noreply.github.com>
2023-05-08 21:28:28 +0900

    modify struct tm
    struct tm 포인터 수정

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm time1, time2;
 
 void filestat1();
 void filestat2();
@@ -95,13 +95,13 @@ void datecmp(){
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
     printf("time compare\n");
-
+    
     // 시간 비교
     if (time1.tm_hour < time2.tm_hour) printf("text1 is early\n");
     else if (time1.tm_hour > time2.tm_hour) printf("text2 is early\n");
-    
     // 분 비교
     else if (time1.tm_min < time2.tm_min) printf("text1 is early\n");
     else if (time1.tm_min > time2.tm_min) printf("text2 is early\n");
+
     else printf("same time\n");
 }
