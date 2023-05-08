6a3b2e8e8f2c1351887105164f88186a18cd473d
9-ch <9ch@kakao.com>
2023-05-08 20:32:07 +0900

    datecamp(작성)

--- main.c
+++ main.c
@@ -78,7 +78,18 @@ void blockcmp(){
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+    printf("date compare\n");
+
+    // 연도 비교
+    if (time1.tm_year < time2.tm_year) printf("text1 is early\n\n");
+    else if(time1.tm_year > time2.tm_year) printf("text2 is early\n\n");
+    // 월 비교
+    else if (time1.tm_mon < time2.tm_mon) printf("text1 is early\n\n");
+    else if (time1.tm_mon > time2.tm_mon) printf("text2 is early\n\n");
+    // 날짜 비교
+    else if (time1.tm_mday < time2.tm_mday) printf("text1 is early\n\n");
+    else if (time1.tm_mday > time2.tm_mday) printf("text2 is early\n\n");
+    else printf("same date\n\n");
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
